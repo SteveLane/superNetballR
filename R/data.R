3924c8e9c84337405a31b2133c39aa279d426615
Steve Lane <lane.s@unimelb.edu.au>
2018-04-07 20:12:40 +1000

    add player data

--- R/data.R
+++ R/data.R
@@ -15,3 +15,21 @@
 #'   \item{game}{Game number of the match}
 #' }
 "season_2017"
+
+#' Season 2017 player data.
+#'
+#' A dataset containing player statistics for all home and away and finals
+#' series matches from the 2017 super netball season, by period.
+#'
+#' @format A data frame with 163336 rows and 8 variables:
+#' \describe{
+#'   \item{playerId}{Unique player number}
+#'   \item{shortDisplayName}{surname, firstname}
+#'   \item{firstname}{Player firstname}
+#'   \item{surname}{Player surname}
+#'   \item{stat}{Statistic measured during the match}
+#'   \item{value}{Value of the statistic}
+#'   \item{round}{Round number of the match}
+#'   \item{game}{Game number of the match}
+#' }
+"players_2017"
