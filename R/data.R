03b96000c8901bf833cba2a45974661518e0895a
Steve Lane <lane.s@unimelb.edu.au>
2018-04-07 19:52:02 +1000

    add season 2017 data

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' Season 2017 match data.
+#'
+#' A dataset containing match statistics for all home and away and finals series
+#' matches from the 2017 super netball season, by period.
+#'
+#' @format A data frame with 15360 rows and 8 variables:
+#' \describe{
+#'   \item{squadId}{Unique squad number}
+#'   \item{squadName}{Full squad name}
+#'   \item{squadNickname}{Squad nickname}
+#'   \item{squadCode}{Short code for quad}
+#'   \item{stat}{Statistic measured during the match}
+#'   \item{value}{Value of the statistic}
+#'   \item{round}{Round number of the match}
+#'   \item{game}{Game number of the match}
+#' }
+"season_2017"
