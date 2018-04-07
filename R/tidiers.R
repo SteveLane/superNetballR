e89b948570ee5a554ff2f63ba9184f31907b161c
Steve Lane <lane.s@unimelb.edu.au>
2018-04-07 20:12:48 +1000

    add player tidier

--- R/tidiers.R
+++ R/tidiers.R
@@ -32,16 +32,17 @@ tidyMatch <- function(match) {
 #' 
 #' @export
 tidyPlayers <- function(match) {
-    team_stats <- match$teamPeriodStats$team
-    team_stats <- dplyr::bind_rows(team_stats)
-    team_info <- match$teamInfo$team
-    team_info <- dplyr::bind_rows(team_info)
-    team_stats <- dplyr::left_join(team_stats, team_info, by = "squadId")
+    player_stats <- match$playerPeriodStats$player
+    player_stats <- dplyr::bind_rows(player_stats)
+    player_info <- match$playerInfo$player
+    player_info <- dplyr::bind_rows(player_info)
+    player_stats <- dplyr::left_join(player_stats, player_info, by = "playerId")
     ## Check if there was overtime (matchInfo)
     final_period <- match$matchInfo$periodCompleted
-    team_stats <- team_stats %>%
+    player_stats <- player_stats %>%
         dplyr::filter(period <= final_period) %>%
-        tidyr::gather(stat, value, -squadId, -squadName,
-                      -squadNickname, -squadCode)
-    team_stats
+        dplyr::select(-displayName) %>%
+        tidyr::gather(stat, value, -playerId, -shortDisplayName, -firstname,
+                      -surname)
+    player_stats
 }
