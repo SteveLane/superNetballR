57f5c3d313a9833e50f64abd283c7b48a624b642
Steve Lane <me@somewhere>
2020-08-08 14:49:44 +1000

    initial commit of shiny examples

---
+++ inst/shiny-examples/global.R
@@ -0,0 +1,13 @@
+################################################################################
+################################################################################
+## Title: Global shiny setup
+## Author: Steve Lane
+## Date: Saturday, 08 August 2020
+## Synopsis: Sets up global libraries and functions for example shiny.
+## Time-stamp: <>
+################################################################################
+################################################################################
+library(here)
+library(dplyr)
+library(ggplot2)
+library(shiny)
